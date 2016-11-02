476c890df067ae31878955692318f947c36da1c8
kate1893 <k.rawlings@soton.ac.uk>
2016-11-02 15:01:03 +0000

    Added rainfall conversion script

---
+++ rainfall_conversion.R
@@ -0,0 +1,11 @@
+#A library to perform rainfall unit conversions
+
+inches_to_mm <- function(inches){
+	#convert inches to millimetres
+	"
+	Arguments: rainfall in inches
+	"
+	mm<-inches * 25.4
+
+	return(mm)
+}
